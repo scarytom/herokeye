7b58fac178727a7bc5a6abaf06ea6633dce2848c
Tom Denley <tom.denley@youdevise.com>
2011-10-31 14:14:54 +0000

    setup local maven repo and main class

---
+++ src/main/java/org/netmelody/herokeye/Herokeye.java
@@ -0,0 +1,9 @@
+package org.netmelody.herokeye;
+
+import org.netmelody.cieye.CiEye;
+
+public final class Herokeye {
+    public static void main(String[] args) throws Exception {
+        CiEye.main(new String[0]);
+    }
+}
