28610b719cd317bbf71f2cd4a309b27dd12b6c5b
scarytom <t.denley@cantab.net>
2011-10-31 22:15:18 +0000

    bind to PORT

--- src/main/java/org/netmelody/herokeye/Herokeye.java
+++ src/main/java/org/netmelody/herokeye/Herokeye.java
@@ -5,7 +5,7 @@ import org.netmelody.cieye.CiEye;
 public final class Herokeye {
     public static void main(String[] args) {
         try {
-            CiEye.main(new String[0]);
+            CiEye.main(new String[] { System.getenv("PORT") });
         }
         catch (Exception e) {
             e.printStackTrace();
